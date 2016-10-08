027e166fc2ca12c3515df63d50aba52271ffb46d
Eakawat Tantamjarik <eakawat2232@gmail.com>
2016-10-08 20:05:14 +0700

    Enable carthage

---
+++ DGActivityIndicator/DGActivityIndicator.h
@@ -0,0 +1,19 @@
+//
+//  DGActivityIndicator.h
+//  DGActivityIndicator
+//
+//  Created by Eakawat Tantamjarik on 10/8/2559 BE.
+//  Copyright © 2559 Danil Gontovnik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DGActivityIndicator.
+FOUNDATION_EXPORT double DGActivityIndicatorVersionNumber;
+
+//! Project version string for DGActivityIndicator.
+FOUNDATION_EXPORT const unsigned char DGActivityIndicatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DGActivityIndicator/PublicHeader.h>
+
+
