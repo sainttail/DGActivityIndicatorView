2767b38167b7692255a1097e9887893eee121a5b
Eakawat Tantamjarik <eakawat2232@gmail.com>
2016-10-19 11:59:51 +0700

    Enable carthage 2

--- DGActivityIndicator/DGActivityIndicator.h
+++ DGActivityIndicator/DGActivityIndicator.h
@@ -15,5 +15,4 @@ FOUNDATION_EXPORT double DGActivityIndicatorVersionNumber;
 FOUNDATION_EXPORT const unsigned char DGActivityIndicatorVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <DGActivityIndicator/PublicHeader.h>
-
-
+#import "DGActivityIndicatorView.h"
